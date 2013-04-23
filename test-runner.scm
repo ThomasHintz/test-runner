5936effe800f52773114adca4b575c0010a872f6
Thomas Hintz <tjhintz@tjhintz-dev.thintz.com>
2013-04-23 04:10:34 +0000

    Split the test-runner out into a more defined lib.

--- test-runner.scm
+++ test-runner.scm
@@ -1,25 +1,14 @@
-#!/usr/bin/csi -script
-;; -*- scheme -*-
+(module test-runner
+ (
+  filter-by compiled src-and-test-files begin-testing
+ )
 
-; to use create a scheme file that defines src-and-test-files
-; then run test-runner tests.scm
-;
-; src-and-test-files needs to return a list of of source files
-; and corresponding test-files
-; ex. '(("lib/foo" "tests/foo"))
-;
-; define tests in a file for each source file
-; the source file will be loaded before the test file is run
-; the test file needs to import the module definitions from
-; the loaded source file
+(import chicken scheme)
+(use test posix srfi-1 srfi-13)
 
-(use test posix srfi-13 srfi-1 easy-args)
-
-(load (car (command-line-arguments)))
-
-(define-arguments
-  ((filter-by f) "")
-  ((compiled c) #f))
+(define filter-by (make-parameter ""))
+(define compiled (make-parameter #f))
+(define src-and-test-files (make-parameter '()))
 
 (define (run-tests test-files suffix)
   (map (lambda (files)
@@ -30,10 +19,13 @@
 (define (test-filter test-files filter-by)
   (filter (lambda (files) (string-contains (car files) filter-by)) test-files))
 
-(test-begin "all")
-(run-tests
- (test-filter
-  (src-and-test-files) ; test-starter must export this
-  (filter-by))
- (if (compiled) "" ".scm"))
-(test-end "all")
+(define (begin-testing)
+  (test-begin "all")
+  (run-tests
+   (test-filter
+    (src-and-test-files)
+    (filter-by))
+   (if (compiled) "" ".scm"))
+  (test-end "all")
+  )
+)
